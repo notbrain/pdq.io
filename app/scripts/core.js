275d78df5ca511bfc39eaac94bb7cfb89660aec6
Brian Ross <brian@bridev.com>
2016-11-03 15:18:45 -0700

    latest wip after a long absence

---
+++ app/scripts/core.js
@@ -0,0 +1,54 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name btcApp
+ * @description
+ * # btcApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('btcApp', [
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ui.router'
+  ])
+  .config(function ($stateProvider) {
+    
+    $stateProvider
+    
+    // $routeProvider
+    //   .when('/', {
+    //     templateUrl: 'views/sanfrancisco.html',
+    //     controller: 'MainCtrl'
+    //   })
+    //   .when('/theme/sf', {
+    //     templateUrl: 'views/sanfrancisco.html',
+    //     controller: 'MainCtrl'
+    //   })
+    //   .when('/theme/proto', {
+    //     templateUrl: 'views/default.html',
+    //     controller: 'MainCtrl'
+    //   })
+    //   .when('/about', {
+    //     templateUrl: 'views/about.html',
+    //     controller: 'AboutCtrl'
+    //   })
+    //   .when('/legacy', {
+    //     templateUrl: 'views/legacy.html',
+    //     controller: 'LegacyCtrl'
+    //   })
+    //   .when('/btc', {
+    //     templateUrl: 'views/btc.html',
+    //     controller: 'BlockchainCtrl'
+    //   })
+    //   .otherwise({
+    //     redirectTo: '/'
+    //   })
+  })
+  .config(function($httpProvider, $locationProvider) {
+    $httpProvider.defaults.useXDomain = true
+    $locationProvider.html5Mode(true);
+  })
